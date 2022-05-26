14b8f204667b2807238c89a66e28267682472aca
carlos.santos <carlos.santos@sp.agence.com.br>
2022-05-26 14:43:10 -0300

    Removendo projeto FronEnd para criar da forma correta

---
+++ FrontEnd/Controllers/MotoristaController.cs
@@ -0,0 +1,41 @@
+﻿using FrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Controllers
+{
+    public class MotoristaController : Controller
+    {
+        public ActionResult Index()
+        {
+            IEnumerable<MotoristaViewModel> clientes = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:61353/api/");
+
+                var responseTask = client.GetAsync("clientes");
+                responseTask.Wait();
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<MotoristaViewModel>>();
+                    readTask.Wait();
+
+                    clientes = readTask.Result;
+                }
+                else
+                {
+                    clientes = Enumerable.Empty<MotoristaViewModel>();
+                    ModelState.AddModelError(string.Empty, "Erro no servidor. Contate o Administrador.");
+                }
+                return View(clientes);
+            }
+        }
+    }
+}
