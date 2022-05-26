1031163f334011e37dcceda8f56ba85582ff4bfc
carlos.santos <carlos.santos@sp.agence.com.br>
2022-05-26 11:23:14 -0300

    Add project files.

---
+++ WebApi/Model/Motorista.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Model
+{
+    public class Motorista
+    {
+        public int Codigo { get; set; }
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+        public int Idade { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
