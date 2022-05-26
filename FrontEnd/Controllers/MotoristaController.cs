cbc8c1a8f596ba3d2e6702bfb7a0649aead89f56
carlos.santos <carlos.santos@sp.agence.com.br>
2022-05-26 15:49:40 -0300

    Criando método Create e o formulário para cadastro de novos motoristas no FrontEnd chamando Web Api; Criando método Edit e o formulário para editar os motoristas no FrontEnd chamando Web Api; Criando método Delete para remover os motoristas no FrontEnd chamando Web Api; Criando método Details para visualizar os motoristas no FrontEnd chamando Web Api;

--- FrontEnd/Controllers/MotoristaController.cs
+++ FrontEnd/Controllers/MotoristaController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
@@ -13,7 +14,7 @@ namespace FrontEnd.Controllers
         // GET: Motorista
         public ActionResult Index()
         {
-            IEnumerable<MotoristaViewModel> clientes = null;
+            IEnumerable<MotoristaViewModel> motoristas = null;
 
             using (var client = new HttpClient())
             {
@@ -28,15 +29,144 @@ namespace FrontEnd.Controllers
                     var readTask = result.Content.ReadAsAsync<IList<MotoristaViewModel>>();
                     readTask.Wait();
 
-                    clientes = readTask.Result;
+                    motoristas = readTask.Result;
                 }
                 else
                 {
-                    clientes = Enumerable.Empty<MotoristaViewModel>();
+                    motoristas = Enumerable.Empty<MotoristaViewModel>();
                     ModelState.AddModelError(string.Empty, "Erro no servidor. Contate o Administrador.");
                 }
-                return View(clientes);
+                return View(motoristas);
             }
+        }
+
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Motorista
+        [HttpPost]
+        public ActionResult Create(MotoristaViewModel motorista)
+        {
+            if (motorista == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:64686/api/");
+
+                var postTast = client.PostAsJsonAsync<MotoristaViewModel>("motorista", motorista);
+                postTast.Wait();
+                var result = postTast.Result;
+
+                if (result.IsSuccessStatusCode)
+                    return RedirectToAction("Index");
+            }
+            ModelState.AddModelError(string.Empty, "Erro no Servidor. Contacte o Administrador.");
+
+            return View(motorista);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? codigo)
+        {
+            if (codigo == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            MotoristaViewModel motorista = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:64686/api/");
+
+                var responseTask = client.GetAsync("motorista/" + codigo.ToString());
+                responseTask.Wait();
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<MotoristaViewModel>();
+                    readTask.Wait();
+
+                    motorista = readTask.Result;
+                }
+            }
+            return View(motorista);
+        }
+
+        // PUT: Motorista
+        [HttpPost]
+        public ActionResult Edit(MotoristaViewModel motorista)
+        {
+            if (motorista == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:64686/api/");
+
+                var putTask = client.PutAsJsonAsync<MotoristaViewModel>("motorista", motorista);
+                putTask.Wait();
+                var result = putTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                    return RedirectToAction("Index");
+            }
+            return View(motorista);
+        }
+
+        // DELTE: Motorista
+        [HttpGet]
+        public ActionResult Delete(int? codigo)
+        {
+            if (codigo == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            MotoristaViewModel motorista = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:64686/api/motorista");
+
+                var deleteTask = client.DeleteAsync("?codigo=" + codigo.ToString());
+                deleteTask.Wait();
+                var result = deleteTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                    return RedirectToAction("Index");
+            }
+            return View(motorista);
+        }
+
+        // GET: Motorista
+        [HttpGet]
+        public ActionResult Details(int? codigo)
+        {
+            if (codigo == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            MotoristaViewModel motorista = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:64686/api/");
+
+                var responseTask = client.GetAsync("motorista/" + codigo.ToString());
+                responseTask.Wait();
+                var result = responseTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<MotoristaViewModel>();
+                    readTask.Wait();
+
+                    motorista = readTask.Result;
+                }
+            }
+            return View(motorista);
         }
     }
 }
